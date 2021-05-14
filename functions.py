ffd9f41df3e1b64fe946b14ee7d5255d00254046
tamjid-ahsan <74892015+tamjid-ahsan@users.noreply.github.com>
2021-05-14 17:49:01 -0400

    init

---
+++ functions.py
@@ -0,0 +1,104 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+def check_NaN(df):
+    """
+    Checks for NaN in the pandas DataFrame and spits a DataFrame of report.
+    Uses df.isnull() method.
+    
+    Parameters:
+    ===========
+    df = pandas.DataFrame
+    """
+    null_checking = []
+    for column in df.columns:
+        not_null = df[column].isnull().value_counts()[0]
+        try:
+            is_null = df[column].isnull().value_counts()[1]
+        except:
+            is_null = 0
+        temp_dict = {'name': column, 'is_null': is_null, 'not_null': not_null}
+        null_checking.append(temp_dict)
+    df_ = pd.DataFrame(null_checking)
+    return df_
+
+def check_duplicates(df, verbose=False):
+    """
+    Checks for duplicates in the pandas DataFrame and return a Dataframe of report.
+    
+    Parameters:
+    ===========
+    df      = pandas.DataFrame
+    verbose = `int` or `boolean`; default: `False`
+                `True` returns DataFrame with details of features.
+                `False` returns DataFrame without details of features.
+    """
+    dup_checking = []
+    for column in df.columns:
+        not_duplicated = df[column].duplicated().value_counts()[0]
+        try:
+            duplicated = df[column].duplicated().value_counts()[1]
+        except:
+            duplicated = 0
+        temp_dict = {
+            'name': column,
+            'duplicated': duplicated,
+            'not_duplicated': not_duplicated
+        }
+        dup_checking.append(temp_dict)
+    df_ = pd.DataFrame(dup_checking)
+    
+    if verbose:
+        for col in df:
+            print(f"""{col}: number of uniques: {len(df[col].unique())}
+                {df[col].unique()}""")
+            print(f"{'_'*60}")
+    return df_
+
+def num_col_for_plotting(row, col=3):
+    """
+    +++ formatting helper function +++
+    __________________________________
+    Returns number of rows to plot
+    
+    Parameters:
+    ===========
+    row = int;
+    col = int; default col: 3
+    """
+    if row % col != 0:
+        return (row // col) + 1
+    else:
+        return row // col
+    
+def df_distribution(df,n_cols=3,fig_size=(16, 26),color_plot='gold',kde_show=True):
+    """
+    Parameters:
+    ===========
+    df          = pandas.DataFrame,
+    n_cols      = int; default: 3
+                    controls number of columns per row of the figure.
+    fig_size    = tuple (length, height); default: (16, 26),
+                    controls the figure size of the output. 
+    color_plot  = str; default: 'gold',
+                    controls color of the histplot and kde plot.
+    kde_show    = `int` or `boolean`; default: `True`
+                    `True` shows kde plot.
+                    `False` does not show kde plot.
+    """
+    fig, axes = plt.subplots(nrows=num_col_for_plotting(len(df.columns),
+                                                            col=n_cols),
+                             ncols=n_cols,
+                             figsize=fig_size,
+                             sharey=False)
+    for ax, column in zip(axes.flatten(), df):
+        sns.histplot(x=column, data=df, color=color_plot, ax=ax, kde=kde_show)
+        ax.set_title(f'Histplot of {column.title()}')
+        ax.tick_params('x', labelrotation=45)
+        sns.despine()
+        plt.tight_layout()
+        plt.suptitle('Histogram plots of the dataset',
+                     fontsize=20,
+                     fontweight=3,
+                     va='bottom')
