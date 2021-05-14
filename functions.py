5f835aa1a758504ab4c2d7a4bbfb87699666810b
tamjid-ahsan <74892015+tamjid-ahsan@users.noreply.github.com>
2021-05-14 19:32:33 -0400

    train-test split

--- functions.py
+++ functions.py
@@ -2,6 +2,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 
+
 def check_NaN(df):
     """
     Checks for NaN in the pandas DataFrame and spits a DataFrame of report.
@@ -22,6 +23,7 @@ def check_NaN(df):
         null_checking.append(temp_dict)
     df_ = pd.DataFrame(null_checking)
     return df_
+
 
 def check_duplicates(df, verbose=False):
     """
@@ -48,13 +50,14 @@ def check_duplicates(df, verbose=False):
         }
         dup_checking.append(temp_dict)
     df_ = pd.DataFrame(dup_checking)
-    
+
     if verbose:
         for col in df:
             print(f"""{col}: number of uniques: {len(df[col].unique())}
                 {df[col].unique()}""")
             print(f"{'_'*60}")
     return df_
+
 
 def num_col_for_plotting(row, col=3):
     """
@@ -71,31 +74,34 @@ def num_col_for_plotting(row, col=3):
         return (row // col) + 1
     else:
         return row // col
-    
-def df_distribution(df,n_cols=3,fig_size=(16, 26),color_plot='gold',kde_show=True):
+
+
+def df_distribution(df, n_cols=3, fig_size=(16, 26), color_plot='gold', kde_show=True, label_rotation=45):
     """
     Parameters:
     ===========
     df          = pandas.DataFrame,
-    n_cols      = int; default: 3
+    n_cols      = int; default: 3,
                     controls number of columns per row of the figure.
     fig_size    = tuple (length, height); default: (16, 26),
                     controls the figure size of the output. 
     color_plot  = str; default: 'gold',
                     controls color of the histplot and kde plot.
-    kde_show    = `int` or `boolean`; default: `True`
+    kde_show    = `int` or `boolean`; default: `True`,
                     `True` shows kde plot.
                     `False` does not show kde plot.
+    label_rotation = int; default: 45,
+                    sets x label rotation.
     """
     fig, axes = plt.subplots(nrows=num_col_for_plotting(len(df.columns),
-                                                            col=n_cols),
+                                                        col=n_cols),
                              ncols=n_cols,
                              figsize=fig_size,
                              sharey=False)
     for ax, column in zip(axes.flatten(), df):
         sns.histplot(x=column, data=df, color=color_plot, ax=ax, kde=kde_show)
         ax.set_title(f'Histplot of {column.title()}')
-        ax.tick_params('x', labelrotation=45)
+        ax.tick_params('x', labelrotation=label_rotation)
         sns.despine()
         plt.tight_layout()
         plt.suptitle('Histogram plots of the dataset',
