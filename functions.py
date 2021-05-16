4b7ee5907581d0ff374cfaa168d1581318cbdb4c
tamjid-ahsan <74892015+tamjid-ahsan@users.noreply.github.com>
2021-05-16 19:37:08 -0400

    baseline binnom_clf done

--- functions.py
+++ functions.py
@@ -1,6 +1,12 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
+import numpy as np
+from sklearn.preprocessing import MinMaxScaler, StandardScaler, OneHotEncoder, RobustScaler, QuantileTransformer, PowerTransformer, MaxAbsScaler, Normalizer
+from sklearn.pipeline import Pipeline
+from sklearn.compose import ColumnTransformer
+from IPython.display import display, HTML
+from sklearn import metrics
 
 
 def check_NaN(df):
@@ -11,6 +17,8 @@ def check_NaN(df):
     Parameters:
     ===========
     df = pandas.DataFrame
+
+    ---version 0.9---
     """
     null_checking = []
     for column in df.columns:
@@ -25,16 +33,22 @@ def check_NaN(df):
     return df_
 
 
-def check_duplicates(df, verbose=False):
+def check_duplicates(df, verbose=False, limit_output=True, limit_num=150):
     """
     Checks for duplicates in the pandas DataFrame and return a Dataframe of report.
     
     Parameters:
     ===========
-    df      = pandas.DataFrame
+    df = pandas.DataFrame
     verbose = `int` or `boolean`; default: `False`
                 `True` returns DataFrame with details of features.
                 `False` returns DataFrame without details of features.
+    limit_output = `int` or `boolean`; default: `True`
+                `True` limits featurs display to 150.
+                `False` details of unique features.
+    limit_num = `int`, limit number of uniques; default: 150,
+
+    ---version 1.2---
     """
     dup_checking = []
     for column in df.columns:
@@ -52,10 +66,16 @@ def check_duplicates(df, verbose=False):
     df_ = pd.DataFrame(dup_checking)
 
     if verbose:
-        for col in df:
-            print(f"""{col}: number of uniques: {len(df[col].unique())}
-                {df[col].unique()}""")
-            print(f"{'_'*60}")
+        if limit_output:
+            for col in df:
+                if (len(df[col].unique()))<=limit_num:
+                    print(f"{col} >> number of uniques: {len(df[col].unique())}\n{df[col].unique()}")
+                else:
+                    print(f"{col} >> number of uniques: {len(df[col].unique())}, showing top {limit_num} values\n{df[col].unique()[:limit_num]}")
+                print(f"{'_'*60}")
+        else:
+            for col in df:
+                print(f"{col} >> number of uniques: {len(df[col].unique())}\n{df[col].unique()}")
     return df_
 
 
@@ -92,6 +112,8 @@ def df_distribution(df, n_cols=3, fig_size=(16, 26), color_plot='gold', kde_show
                     `False` does not show kde plot.
     label_rotation = int; default: 45,
                     sets x label rotation.
+    
+    ---version 1.1---
     """
     fig, axes = plt.subplots(nrows=num_col_for_plotting(len(df.columns),
                                                         col=n_cols),
@@ -108,3 +130,195 @@ def df_distribution(df, n_cols=3, fig_size=(16, 26), color_plot='gold', kde_show
                      fontsize=20,
                      fontweight=3,
                      va='bottom')
+
+
+def df_preprocessing_pipeline(df, scaler=StandardScaler(), drop=None):
+    """
+    Seperates DataFrame by categorical and numerical coulmns, and performs OneHotEncoding with droping control on categorical coulumns and scaling on numerical columns, user can select scalers. Then returns a transformed DataFrame.
+    All steps are done using sklearn pipelines and transformers. 
+    
+    Parameters:
+    ===========
+    df     = pandas.DataFrame object.
+    scaler = sklarn scaler object; default: StandardScaler(),
+                *** IMPORT desired scaler before using. ***
+                *** OR call with this module. all of them are imported and ready 
+                to use inside this module.***
+                Available options:
+                - StandardScaler: removes the mean and scales the data to 
+                    unit variance. 
+                - MinMaxScaler: rescales the data set such that all feature 
+                    values are in the range [0, 1]
+                - RobustScaler: is based on percentiles and are therefore not
+                    influenced by a few number of very large marginal outliers.
+                - QuantileTransformer: applies a non-linear transformation 
+                    such that the probability density function of each feature
+                    will be mapped to a uniform or Gaussian distribution.
+                - PowerTransformer: applies a power transformation to each 
+                    feature to make the data more Gaussian-like in order to 
+                    stabilize variance and minimize skewness.
+                - MaxAbsScaler: is similar to `MinMaxScaler` except that the
+                    values are mapped in the range [0, 1]
+                - Normalizer: rescales the vector for each sample to have 
+                    unit norm, independently of the distribution of the samples.
+    drop   = str or `None`, Option to control OHE droping; default: None.
+                - None : retain all features (the default).
+                - 'first' : drop the first category in each feature. If only one
+                  category is present, the feature will be dropped entirely.
+                - 'if_binary' : drop the first category in each feature with two
+                  categories. Features with 1 or more than 2 categories are
+                  left intact.
+                - array : ``drop[i]`` is the category in feature ``X[:, i]`` that
+                  should be dropped.
+    
+    Next steps: use OOP to make this a class.
+
+    ---version 0.9.1---
+    """
+    # isolating numerical features
+    nume_cols = df.select_dtypes('number').columns.to_list()
+    # isolating categorical features
+    cate_cols = df.select_dtypes('category').columns.to_list()
+    # pipeline for processing categorical features
+    pipe_cate = Pipeline([('ohe', OneHotEncoder(sparse=False, drop=drop))])
+    # pipeline for processing numerical features
+    pipe_nume = Pipeline([('scaler', scaler)])
+    # Coulmn transformer
+    preprocessor = ColumnTransformer([
+        ('numerical_features', pipe_nume, nume_cols),
+        ('categorical_features', pipe_cate, cate_cols)
+    ])
+    # creating a pandas.DataFrame with appropriate header
+    ret = pd.DataFrame(
+        preprocessor.fit_transform(df),
+        columns=nume_cols +
+        preprocessor.named_transformers_['categorical_features'].
+        named_steps['ohe'].get_feature_names(cate_cols).tolist())
+    return ret
+
+def coefficients_of_model_binary(model,X_train_data, log_scale=True):
+    """
+    Returns a pandas.Series object with intercept and coeffients of a logistic regression model with features as index
+    
+    Parameters:
+    ===========
+    model        = object; No Default. 
+                    fitted sklearn model object with a coef_ and intercept_ attribute.
+    X_train_data = pandas.DataFrame; No Default.
+                    DataFrame of independant variables. Should be train-test splitted. 
+                    Use train data.  
+    log_scale    = boolean; default: True.
+                    `True` for keeping log scale of coefficients.
+                    `False` for converting to normal scale.
+
+    """
+    coeffs = pd.Series(model.coef_.flatten(), index=X_train_data.columns)
+    coeffs['intercept'] = model.intercept_[0]
+    if log_scale is False:
+        coeffs = np.exp(coeffs)
+    return coeffs
+
+def coefficients_of_model(model,X_train_data,log_scale=True):
+    """
+    Returns a pandas.Series object with intercept and coeffients.
+    Parameters:
+    ===========
+    model        = object; No Default. 
+                    fitted sklearn model object with a coef_ and intercept_ attribute.
+    X_train_data = pandas.DataFrame; No Default.
+                    DataFrame of independant variables. Should be train-test splitted. 
+                    Use train data.  
+    log_scale    = boolean; default: True.
+                    `True` for keeping log scale of coefficients.
+                    `False` for converting to normal scale.
+
+    """
+    coeffs = pd.Series(model.coef_.flatten())
+    coeffs['intercept'] = model.intercept_[0]
+    if log_scale is False:
+        coeffs = np.exp(coeffs)
+    return coeffs
+
+def model_report(model,
+                 X_train,
+                 y_train,
+                 X_test,
+                 y_test,
+                 cmap='Greens',
+                 normalize='true',
+                 figsize=(16, 6)):
+    """
+    Report of model using train-test split dataset.
+    Shows train and test score, Confusion Matrix of test data and, ROC Curve of test data.
+    
+    Parameters:
+    ===========
+    model     = object, scikit-learn model object; no default.
+    X_train   = pandas.DataFrame, predictor variable training data split; no default,
+    y_train   = pandas.DataFrame, target variable training data split; no default,
+    X_test    = pandas.DataFrame, predictor variable test data split; no default,
+    y_test    = pandas.DataFrame, target variable test data split; no default,
+    cmap      = str, colormap of Confusion Matrix; default: 'Greens',
+    normalize = str, normalize count of Confusion Matrix; default: 'true',
+                `true` to normalize.
+                `false` to show counts.
+    figsize   = tuple ``(lenght, height)``, figsize of output; default: (16, 6),
+    
+    ---version 0.9---
+    """
+    
+    def str_model_(model):
+        """Helper function to get model class display statement, this text conversion breaks code if performed in that local space. This is to isolate from the function local space."""
+        str_model = str(model)
+        display(
+            HTML(
+                f"""<strong>Report of {str_model.split('(')[0]} type model using train-test split dataset.</strong>"""
+            ))
+
+    str_model_(model)
+    model.fit(X_train, y_train)
+    print(f"{'*'*90}")
+    train = model.score(X_train, y_train)
+    test = model.score(X_test, y_test)
+    print(f"""Train score: {train.round(4)}""")
+    print(f"""Test score: {test.round(4)}""")
+    if abs(train - test) <= .05:
+        print(
+            f"    No over or underfitting detected, diffrence of scores did not cross 5% thresh hold."
+        )
+    elif (train - test) > .05:
+        print(
+            f"    Possible Overfitting, diffrence of scores {abs(train-test).round(4)*100}% crossed 5% thresh hold."
+        )
+    elif (train - test) < -.05:
+        print(
+            f"    Possible Underfitting, diffrence of scores {abs(train-test).round(4)*100}% crossed 5% thresh hold."
+        )
+    print(f"{'*'*90}")
+    print("")
+    print(f"{'*'*60}")
+    print(f"""Classification report of:
+    {model}""")
+    print(f"{'-'*60}")
+    print(metrics.classification_report(y_test, model.predict(X_test)))
+    print(f"{'*'*60}")
+
+    fig, ax = plt.subplots(nrows=1, ncols=2, figsize=figsize)
+    metrics.plot_confusion_matrix(model,
+                                  X_test,
+                                  y_test,
+                                  cmap=cmap,
+                                  normalize=normalize,
+                                  ax=ax[0])
+    ax[0].title.set_text('Confusion Matrix')
+    metrics.plot_roc_curve(model,
+                           X_test,
+                           y_test,
+                           color='gold',
+                           ax=ax[1])
+    ax[1].plot([0, 1], [0, 1], ls='-.', color='white')
+    ax[1].grid()
+    ax[1].title.set_text('ROC Curve')
+
+    plt.tight_layout()
+    plt.show()
